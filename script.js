dd901236487331ca9fba6efa972d4c4aeadcaa42
JDPescosta <jamesdpescosta@gmail.com>
2019-12-18 10:06:27 -0800

    updated the grid for dynamic input, finished project

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-const gridSize = 16*16; // set this as the square of one of the grid's sides e.g. gridSize = 64*64
-const tileSize = 32;
-const container = document.querySelector('#container')
-container.style.cssText = `width: ${tileSize*Math.sqrt(gridSize)}px; height: ${tileSize * (gridSize/gridSize)}px; `
+let squaresPerSide = 16; // the number of squares per a side of the drawing grid
+const containerSide = 640; // the length of the container's side in pixels.
+const container = document.querySelector("#container"); 
+const reset = document.createElement("button");
+const header = document.querySelector("header");
 
-for(let i = 0; i < gridSize; i++){
-    const gridTile = document.createElement('div');
-    gridTile.classList.add('grid-tile')
-    gridTile.style.cssText = `width: ${tileSize}px; height: ${tileSize}px`;
+header.appendChild(reset);
+
+container.style.cssText = `width: ${containerSide}px; height: ${containerSide}px; `;
+
+const gridGen = () => {
+  const gridSize = squaresPerSide * squaresPerSide;
+  const tileSide = containerSide / squaresPerSide;
+
+  for (let i = 0; i < gridSize; i++) {
+    const gridTile = document.createElement("div");
+    gridTile.classList.add("grid-tile");
+    gridTile.style.cssText = `width: ${tileSide}px; height: ${tileSide}px`;
+    gridTile.addEventListener("mouseover", () => {
+      gridTile.style.backgroundColor = "white";
+    });
     container.appendChild(gridTile);
-}
+  }
+};
+
+gridGen();
+
+reset.textContent = "Reset the grid.";
+
+reset.addEventListener("click", () => {
+  let tiles = document.querySelectorAll(".grid-tile");
+  tiles.forEach(tile => {
+    container.removeChild(tile);
+  });
+  squaresPerSide = prompt(
+    "How many squares per a side would you like the new etch-a-sketch grid to be?", 16
+  );
+  gridGen();
+});
